9bdad7bf6b9879627bb57fc48d194ccf9405c31b
uzzal408 <imail2226@diu.edu.bd>
2022-09-22 15:42:44 +0600

    link insert

--- linklist.py
+++ linklist.py
@@ -1,17 +1,40 @@
 #class node
 class Node:
-    def __int__(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 #class link list
 class LinkedList:
-    def __int__(self):
+    def __init__(self):
         self.head = None
+    #traverse node
     def printList(self):
         temp = self.head
         while(temp):
             print(temp.data)
             temp = temp.next
+    #add new node at front
+    def push(self,new_data):
+        #1 & 2 allocate a new node and put in the data
+        new_node = Node(new_data)
+        #3 make next of new node as Head
+        new_node.next = self.head
+        #4 move to the head poin to new node
+        self.head = new_node
+    def insertAfter(self, prev_node,new_data):
+        if prev_node is None:
+            print('Previous node must in the list')
+            return
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -29,4 +52,11 @@ if __name__ == '__main__':
     llist.head.next = second
     second.next     = third
 
+    llist.push(4)
+    print('after push node')
+    llist.printList()
+    prev_node = Node(6)
+    print(prev_node.next)
+    print('after insert specific location node',llist.head.next)
+    llist.insertAfter(llist.head.next, 5)
     llist.printList()
